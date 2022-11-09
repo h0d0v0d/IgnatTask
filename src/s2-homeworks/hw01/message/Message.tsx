ea090198ccfb645f762cc399faf53c604ca0a758
Егор Серафонтов <pixaretyiypop@gmail.com>
2022-11-10 01:11:19 +0300

    sceleton of message

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import s from './Message.module.css'
+import { MessageType } from '../HW1'
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+    message: MessageType
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
-                <img
+                <img 
                     id={'hw1-avatar-' + props.message.id}
                     // создаёт студент
-
+                    src={props.message.user.avatar}
+                    alt='avatar'
                     //
                 />
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
-
+                        <h2>{props.message.user.name}</h2>
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
-
+                        <h2>{props.message.message.text}</h2> 
                         {/**/}
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
-
+                <h4>{props.message.message.time}</h4>
                 {/**/}
             </div>
         </div>
