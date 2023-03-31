bcd8e8ca31979a21b0e61dc695885f263c08d625
Егор Серафонтов <pixaretyiypop@gmail.com>
2023-03-31 20:25:36 +0300

    hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,16 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type InitialStateType = typeof initState
+export type ActionType = {type: 'SET_THEME_ID', id: number}
+
+export const themeReducer = (state: InitialStateType = initState, action: ActionType): InitialStateType => { // fix any
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID': return {themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ActionType => ({ type: 'SET_THEME_ID', id }) // fix any
